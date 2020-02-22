d5c59c1d3234e9ffa8ee4e74351d452f9a6b67fb
fahadbinalam126 <fahad24hr@gmail.com>
2020-02-21 16:09:28 -0800

    Commit Hello World!

---
+++ src/main/java/com/example/getmesocialsevice/GetmesocialSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.getmesocialsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmesocialSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmesocialSeviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/getmesocialsevice/GetmesocialSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getmesocialsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmesocialSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
